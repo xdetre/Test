190eacf07d094bf1e5321fe9dddc9a90f9eaf812
xdetre <enderctoun@gmail.com>
2025-01-26 22:20:49 +0300

    created new project

---
+++ test.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self, name, old):
+        self.__name = name
+        self.__old = old
+
+    @property
+    def old(self):
+        return self.__old
+
+    @old.setter
+    def old(self, old):
+        self.__old = old
+
+    @old.deleter
+    def old(self):
+        del self.__old
+
+    # old = property()
+    # old = old.setter(set_old)
+    # old = old.getter(get_old)
+
+p = Person('Artur', 20)
+p.__dict__['old'] = 'old in obj p'
+a = p.old
+p.old = 10
+print(p.old, p.__dict__)
